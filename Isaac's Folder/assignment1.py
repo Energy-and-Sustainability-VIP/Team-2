2db371503d1135df807e396d502f454e40bc8c3c
Isaac Towai <towaii@hawaii.edu>
2025-03-05 14:27:03 -1000

    Assignment 1

---
+++ Isaac's Folder/assignment1.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df = pd.read_csv("Watts, 3-Ph total.csv")
+
+max_row = df.loc[df["meter_reading"].idxmax()]
+min_row = df.loc[df["meter_reading"].idxmax()]
+average_value = df["meter_reading"].mean()
+
+print("Max Meter Reading Row:\n", max_row, "\n")
+print("Min Meter Reading Row:\n", min_row, "\n")
+print(f"Average Meter Reading: {average_value:.2f}\n")
+
+df["kilowatts"] = df["meter_reading"] / 1000
+df["kWh"] = df["kilowatts"] * 0.25
+df.to_csv("Isaac_Towai's Folder/AssignmentOne.csv", index = False)
